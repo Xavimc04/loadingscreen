60d4a451ed49215c9d5cd85de0ca3243bbc02f2b
Xavimc04 <xaviermorcam@campus.monlau.com>
2024-04-18 19:15:24 +0200

    Loading process fivem events

--- interface/src/components/game-percent.tsx
+++ interface/src/components/game-percent.tsx
@@ -1,11 +1,47 @@
+import { useState, useEffect } from "react";
+
 export default function GamePercent() {
+    const [progress, handleProgress] = useState(0); 
+
+    useEffect(() => {
+        var count = 0;
+        var thisCount = 0;
+        
+        const handlers: {
+            [key: string]: (data: any) => void
+        } = {
+            startInitFunctionOrder(data: { count: number }) {
+                count = data.count;
+            },
+            initFunctionInvoking(data: { idx: number }) {
+                handleProgress(((data.idx / count) * 100)); 
+            },
+            startDataFileEntries(data: { count: number }) {
+                count = data.count;
+            },
+            performMapLoadFunction(data: { name: string }) {
+                ++thisCount;
+                handleProgress(((thisCount / count) * 100)); 
+            }
+        };
+        
+        window.addEventListener('message', function (e: { data: { eventName: string }}) {
+            (handlers[e.data.eventName] || function () { })(e.data);
+        });
+    }, []);
+
     return <article 
         className="border-4 border-indigo-500 text-white relative flex flex-col gap-2 justify-center px-8 h-28 w-full"
         style={{
             boxShadow: "inset 0 0 70px rgba(99, 102, 241, 0.5)"
         }}
     >
-        <div className="h-full w-4/6 bg-indigo-500 absolute left-0 -z-10"></div>
+        <div 
+            className="h-full bg-indigo-500 absolute left-0 -z-10"
+            style={{
+                width: `${ progress }%`
+            }}
+        ></div>
 
         <h2
             className="poppins text-xl"
