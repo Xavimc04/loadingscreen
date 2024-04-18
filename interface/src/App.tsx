784666c83365f790c3e41a216ea2a736613e98b3
Xavimc04 <xaviermorcam@campus.monlau.com>
2024-04-18 15:56:56 +0200

    Disable website scrolling

--- interface/src/App.tsx
+++ interface/src/App.tsx
@@ -1,14 +1,12 @@
 import Video from "./components/video";
 
 export default function Page() {
-    return <main className='h-screen w-screen bg-red-500 flex'>
+    return <main className='h-screen w-screen'>
         <Video />
 
-        {/* @ Server information */}
-        <section>
-            <h1>
-                
-            </h1>
-        </section>
+        {/* @ Loading content */}
+        <div className="bg-black/70 backdrop-blur-md absolute z-20 top-0 left-0 w-screen h-screen">
+            Loading content...
+        </div>
     </main>
 }
